cfe4203c22ba43b5f41eb3d8d7905a686cb78f67
DAVID PC-CAJA <davidortiz25122010@gmail.com>
2023-10-25 14:19:28 -0400

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>D & C &mdash; Mi boda</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
+
+  <!--
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE
+	DESIGNED & DEVELOPED by FREEHTML5.CO
+
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="{{asset('css/animate.css')}}">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="{{asset('css/icomoon.css')}}">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+	<link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+	<!-- Modernizr JS -->
+	<script src="{{asset('js/modernizr-2.6.2.min.js')}}"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+	<div class="fh5co-loader"></div>
+
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-2">
+					<div id="fh5co-logo"><a href="#">Mi Boda<strong>.</strong></a></div>
+				</div>
+				<div class="col-xs-10 text-right menu-1">
+					<ul>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+	</nav>
+
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image: images/img_bg_3.jpg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>Derlis &amp; Cludia</h1>
+							<h2>Nos Casamos</h2>
+							<div class="simply-countdown simply-countdown-one"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="fh5co-gallery" class="fh5co-section-gray">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+					<h2>Regalos Deseados</h2>
+					<p>Agenda de regalos para los buenos deseos para la pareja de Derlis y Claudia.</p>
+				</div>
+			</div>
+			<div class="row row-bottom-padded-md">
+				<div class="col-md-12">
+					<ul id="fh5co-gallery-list">
+
+                        <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url('regalos/cafetera.jpg'); ">
+                            <a class="color-4">
+                                <div class="case-studies-summary">
+                                    <span style="">Disponile</span>
+                                    <h2>Cafetera</h2>
+                                    <button type="button">Reservar</button>
+                                    </span>
+                                </div>
+                            </a>
+                        </li>
+
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="{{asset('js/jquery.min.js')}}"></script>
+	<!-- jQuery Easing -->
+	<script src="{{asset('js/jquery.easing.1.3.js')}}"></script>
+	<!-- Bootstrap -->
+	<script src="{{asset('js/bootstrap.min.js')}}"></script>
+	<!-- Waypoints -->
+	<script src="{{asset('js/jquery.waypoints.min.js')}}"></script>
+	<!-- Carousel -->
+	<script src="{{asset('js/owl.carousel.min.js')}}"></script>
+	<!-- countTo -->
+	<script src="{{asset('js/jquery.countTo.js')}}"></script>
+
+	<!-- Stellar -->
+	<script src="{{asset('js/jquery.stellar.min.js')}}"></script>
+	<!-- Magnific Popup -->
+	<script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+	<script src="{{asset('js/magnific-popup-options.js')}}"></script>
+
+	<!-- // <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js"></script> -->
+	<script src="{{asset('js/simplyCountdown.js')}}"></script>
+	<!-- Main -->
+	<script src="{{asset('js/main.js')}}"></script>
+
+	<script>
+    // var d = new Date(new Date().getTime() + 200 * 120 * 120 * 2000);
+
+    var fechaActual = new Date();
+
+    // Definir la fecha de la boda
+    var fechaBoda = new Date('12/23/2023');
+
+    // Calcular la diferencia en milisegundos
+    var diferenciaEnTiempo = fechaBoda.getTime() - fechaActual.getTime();
+
+    // Calcular la diferencia en días
+    var diferenciaEnDias = Math.ceil(diferenciaEnTiempo / (1000 * 3600 * 24));
+
+    // Sumar la diferencia de días a la fecha actual
+    var d = new Date(fechaActual.getTime() + diferenciaEnDias * (24 * 60 * 60 * 1000));
+
+    // default example
+    simplyCountdown('.simply-countdown-one', {
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate()
+    });
+
+    //jQuery example
+    $('#simply-countdown-losange').simplyCountdown({
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate(),
+        enableUtc: false
+    });
+</script>
+
+	</body>
+</html>
+
